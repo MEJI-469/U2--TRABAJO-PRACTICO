dc6c5e5f4f2b02a3ad5b262ada02bd9cb165b11b
MEJI-469 <125814121+MEJI-469@users.noreply.github.com>
2024-11-25 17:49:12 -0500

    .
    .

---
+++ ProyectoUsuario/src/main/java/com/crud/demo/services/IUsuarioService.java
@@ -0,0 +1,14 @@
+package com.crud.demo.services;
+
+import java.util.List;
+
+import com.crud.demo.entity.Usuario;
+
+public interface IUsuarioService {
+
+public List<Usuario> listarTodo();
+public Usuario  save(Usuario usuarios);
+public  Usuario buscarPorID(Long id);
+public void Eliminar(Long id);
+	
+}
